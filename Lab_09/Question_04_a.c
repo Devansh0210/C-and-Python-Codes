9ea02295b8c2c6fb33963f8d07b6a61451e9becd
Devansh0210 <51352071+Devansh0210@users.noreply.github.com>
2019-10-16 22:35:57 +0530

    Create Question_04_a.c

---
+++ Lab_09/Question_04_a.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void occ(char list[],int n){
+
+
+
+}
+
+
+void main(){
+
+        char s[20];
+        int i,N;
+        printf("Enter the string : ");
+//      scanf("%d",&N);
+        i=0;
+
+
+        s[0]=getchar();
+        while(s[i]!='\n'){
+                i++;
+                s[i]=getchar();
+
+                }
+//      s[++i]='\0';
+        for
+        printf("%s\n",s);
+
+
+
+}
