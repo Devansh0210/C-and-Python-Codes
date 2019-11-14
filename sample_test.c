4b41b1158fafc19d900bee42a4800b79e244869d
Devansh0210 <51352071+Devansh0210@users.noreply.github.com>
2019-11-14 19:19:14 +0530

    Create sample_test.c

---
+++ sample_test.c
@@ -0,0 +1,40 @@
+//***************************************************************
+//******* PLEASE FILL THESE DETAILS AND COMMENT THE LINES *******
+//** UNLESS YOU COMMENT THESE LINES, THE CODE WILL NOT COMPILE **
+ Name:
+ Id no.:
+ Terminal no.:
+ Lab no.: 
+//***************************************************************
+//***************************************************************
+
+#include<stdio.h>
+#include<string.h>
+/////////// DO NOT MODIFY ANYTHING in main() ////////
+char* search(char*, int);
+void main()
+{
+char * p;
+char c;
+char s[20];
+printf("This code will run for five strings\n");
+for (int i=0; i<5; i++)
+{
+        scanf("%s %c",s,&c);
+        p = search(s, c);
+        printf("new string: %s\n", p);
+        }
+}
+///////////////////////////////////////////////
+//DO NOT MODIFY ANYTHING ABOVE THIS LINE
+///////////////////////////////////////////////
+
+//// Write your code below /////
+char* search(char* str,int ch){
+    
+    str=strchr(str,ch);
+    str=str+1;
+    str=strchr(str,ch);
+    
+    return str;
+}
