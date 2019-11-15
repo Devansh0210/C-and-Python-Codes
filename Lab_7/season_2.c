184521ebd591a4de5cb35f13416d6f3612973cbe
Devansh0210 <51352071+Devansh0210@users.noreply.github.com>
2019-11-15 23:17:06 +0530

    Create season_2.c

---
+++ Lab_7/season_2.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+void main(){
+
+        int s;
+        printf("Enter month no. : ");
+        scanf("%d",&s);
+
+        switch(s)
+        {
+        case 12:
+                printf("the season is Winter...");
+                break;
+
+        case 1:
+                printf("the season is Winter...");
+                break;
+        case 2:
+                printf("the season is Winter...\n");
+                break;
+        case 3:
+                printf("the season is Spring...\n");
+                break;
+        case 4:
+                printf("the season is Spring...\n");
+                break;
+        case 5:
+                printf("the season is Spring...\n");
+                break;
+        case 6:
+                printf("the season is Summer...\n");
+                break;
+        case 7:
+                printf("the season is Summer...\n");
+                break;
+        case 8:
+                printf("the season is Summer...\n");
+                break;
+        case 9:
+                printf("the season is Autumn...\n");
+                break;
+        case 10:
+                printf("the season is Autumn...\n");
+}
+}  
+
+
