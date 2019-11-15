df60de8a300d31f71ff98463cb74ae7b083a5375
Devansh0210 <51352071+Devansh0210@users.noreply.github.com>
2019-11-15 23:15:01 +0530

    Create palindrome.c

---
+++ Lab_7/palindrome.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+
+        char n[4];
+        printf("Enter the four digit no. : ");
+        scanf("%s",n);
+        n[0]==n[3] && n[2]==n[1] ? printf("The no. is palindrome ...\n") : printf("The no. is not palindrome ...\n ");
+
+
+
+}
+
+
